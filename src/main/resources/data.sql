de18ecb74c11fb276c26ff3edd214b02d9e2ed45
Mdsoln <muddyfakih98@gmail.com>
2025-05-08 14:44:04 +0300

    added dev && prod application configurations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Persons (
+                         PersonID int,
+                         LastName varchar(255),
+                         FirstName varchar(255),
+                         Address varchar(255),
+                         City varchar(255)
+);
+-- INSERT INTO `tablename`(Id, Name) VALUES(1, "Name sample");
