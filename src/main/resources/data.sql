2edac9e8790cf040a54551f5902155f5b193868b
Mdsoln <muddyfakih98@gmail.com>
2025-05-08 15:03:12 +0300

    adding data.sql sample data files with users, donors, hospitals && slots

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,5 @@ CREATE TABLE Persons (
                          Address varchar(255),
                          City varchar(255)
 );
--- INSERT INTO `tablename`(Id, Name) VALUES(1, "Name sample");
+
+-- INSERT INTO `user`('password','roles','username') VALUES("1", "Name","sample");
